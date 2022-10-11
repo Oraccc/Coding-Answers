2ac619b54387173f56cbb1229982a1d1b428bdeb
Yang Xu <xuyang2018@sjtu.edu.cn>
2022-10-11 22:59:43 +0800

    update solutions

---
+++ Chapter 13/Ex13-43/StrVec.h
@@ -0,0 +1,51 @@
+#ifndef STRVEC_H
+#define STRVEC_H
+
+#include <string>
+#include <memory>
+#include <initializer_list>
+
+using namespace std;
+
+class StrVec {
+public:
+    typedef size_t size_type;
+    StrVec() : first_elem(nullptr), first_free(nullptr), cap(nullptr) { }
+    StrVec(initializer_list<string>);
+
+    StrVec(const StrVec &);
+    ~StrVec();
+    StrVec &operator=(const StrVec &);
+
+    void push_back(const string &s);
+    void pop_back();
+
+    void reserve(size_type);
+    void resize(size_type, const string & = string());
+
+    bool empty() const { return cbegin() == cend(); }
+    size_type size() const { return first_free - first_elem; }
+    size_type capacity() const { return cap - first_elem; }
+
+    string *begin() { return first_elem; }
+    string *end() { return first_free; }
+    const string *begin() const { return first_elem; }
+    const string *end() const { return first_free; }
+    const string *cbegin() const { return begin(); }
+    const string *cend() const { return end(); }
+
+private:
+    static allocator<string> alloc;
+
+    string *first_elem;
+    string *first_free;
+    string *cap;
+
+    void chk_n_alloc() { if (size() == capacity()) reallocate(); }
+    pair<string *, string *>
+    alloc_n_copy(const string *, const string *);
+    void reallocate();
+    void free();
+};
+
+#endif
---
+++ Chapter 13/Ex13-48/String.cpp
@@ -0,0 +1,109 @@
+#include "String.h"
+#include <iostream>
+
+using namespace std;
+
+allocator<char> String::alloc;
+
+String::String(const_iterator ci) : String() 
+{
+    cout << "String::String(const_iterator ci)" << endl;
+    while (ci && *ci != '\0')
+        push_back(*ci++);
+}
+
+String::String(initializer_list<char> il) : String() 
+{
+    for (const auto &c : il)
+        push_back(c);
+}
+
+void String::push_back(const_reference c) 
+{
+    if (size() == capacity())
+        reallocate(empty() ? 1 : 2 * capacity());
+    alloc.construct(first_free++, c);
+}
+
+void String::pop_back() 
+{
+    if (!empty())
+        alloc.destroy(--first_free);
+}
+
+void String::reserve(size_type n) 
+{
+    if (n > capacity()) 
+    {
+        reallocate(n);
+    }
+}
+
+void String::resize(size_type n, const_reference c) 
+{
+    if (n > size()) 
+    {
+        for (int i = n - size(); i > 0; --i)
+        push_back(c);
+    } 
+    else if (n < size()) 
+    {
+        for (int i = size() - n; i > 0; --i)
+        pop_back();
+    }
+}
+
+void String::reallocate(size_type n) 
+{
+    auto new_first_elem = alloc.allocate(n);
+    auto new_first_free = new_first_elem;
+    for (auto it = begin(); it != end(); ++it)
+        alloc.construct(new_first_free++, std::move(*it));
+    free();
+    first_elem = new_first_elem;
+    first_free = new_first_free;
+    cap = first_elem + n;
+}
+
+void String::free() 
+{
+    while (!empty())
+        alloc.destroy(--first_free);
+    alloc.deallocate(first_elem, capacity());
+    first_elem = first_free = cap = nullptr;
+}
+
+String::String(const String &rhs) 
+{
+    cout << "String::String(const String &rhs)" << endl;
+    first_elem = alloc.allocate(rhs.size());
+    first_free = cap = uninitialized_copy(rhs.begin(), rhs.end(), first_elem);
+}
+
+String::~String() 
+{
+    free();
+}
+
+String &String::operator=(const String &rhs) 
+{
+    cout << "String &String::operator=(const String &rhs)" << endl;
+    auto new_first_elem = alloc.allocate(rhs.size());
+    auto new_first_free = uninitialized_copy(rhs.begin(), rhs.end(), new_first_elem);
+    free();
+    first_elem = new_first_elem;
+    first_free = cap = new_first_free;
+    return *this;
+}
+
+String &String::operator=(const_iterator ci) 
+{
+    cout << "String &String::operator=(const_iterator ci)" << endl;
+    *this = String(ci);
+    return *this;
+}
+
+string String::str() const 
+{
+    return string(cbegin(), cend()); 
+}
